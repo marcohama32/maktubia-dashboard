e94e1c0ba91c850d496efad4bf5fb9aa98f976e9
Marco Hama <marco@marcohama.com>
2026-01-13 20:48:06 +0200

    Atualizar logo do sidebar para logo3.png e aumentar tamanho

--- src/dashboard/sidebar/SidebarHeader.tsx
+++ src/dashboard/sidebar/SidebarHeader.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 export function SidebarHeader() {
   return (
     <div className="sticky top-0 z-10 flex items-center justify-center bg-white py-6">
-      <div className="relative h-[60px] w-[180px]">
+      <div className="relative h-[150px] w-[300px]">
         <Image
-          src="/images/logo2.png"
+          src="/images/logo3.png"
           alt="Maktubia Logo"
           fill
           className="object-contain"
           priority
-          sizes="180px"
+          sizes="300px"
         />
       </div>
     </div>
